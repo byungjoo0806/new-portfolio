d8e6cf6439179d32fdc68d13a03be3d7fbf3a577
byungjoo0806 <andybyungjoopark@gmail.com>
2024-03-24 21:42:15 +0900

    still fixing deploy issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router basename='/new-portfolio'>
       <Routes>
         <Route path='/' element={<Home />}></Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 

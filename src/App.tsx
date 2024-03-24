227906602973280604615c36d2895a4d9e3f1aff
byungjoo0806 <andybyungjoopark@gmail.com>
2024-03-24 21:34:46 +0900

    deploy error fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Intro from './pages/Intro';
 import Home from './pages/Home';
 
 function App() {
@@ -7,7 +6,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        {/* <Route path='/home' element={<Home />}></Route> */}
       </Routes>
     </BrowserRouter>
   );

07bbc37abfe1756d6a04dedbb61b91df271b70c9
byungjoo0806 <andybyungjoopark@gmail.com>
2024-03-24 21:24:45 +0900

    small error fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Intro />}></Route>
-        <Route path='/home' element={<Home />}></Route>
+        <Route path='/' element={<Home />}></Route>
+        {/* <Route path='/home' element={<Home />}></Route> */}
       </Routes>
     </BrowserRouter>
   );

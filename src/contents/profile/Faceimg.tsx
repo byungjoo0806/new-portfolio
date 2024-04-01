b9e7aaf05523b3908b80af4795640a12d485bfab
byungjoo0806 <andybyungjoopark@gmail.com>
2024-04-01 20:37:39 +0900

    small error fix in profile

--- src/contents/profile/Faceimg.tsx
+++ src/contents/profile/Faceimg.tsx
@@ -5,8 +5,8 @@ type Props = {}
 const Faceimg = (props: Props) => {
   return (
     <div className='w-[50%] md:w-[25%] h-[90%] rounded-full overflow-hidden'>
-        {/* <img className='w-full h-full' src="./profile_pic.jpeg" alt="profile pic" /> */}
-        <img className='w-full h-full' src='https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&q=70&fm=webp' alt='testing' />
+        <img className='w-full h-full' src="./profile_pic.jpeg" alt="profile pic" />
+        {/* <img className='w-full h-full' src='https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&q=70&fm=webp' alt='testing' /> */}
     </div>
   )
 }

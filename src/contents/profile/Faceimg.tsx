e0c3db25735d24b76054833bae9c4d8d12d589a7
byungjoo0806 <andybyungjoopark@gmail.com>
2024-04-01 20:24:10 +0900

    profile area design fixing

--- src/contents/profile/Faceimg.tsx
+++ src/contents/profile/Faceimg.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Faceimg = (props: Props) => {
   return (
-    <div className='w-[50%] md:w-[25%] rounded-full border border-black'>
+    <div className='w-[50%] md:w-[25%] rounded-full border border-black overflow-hidden'>
         <img className='w-full h-full' src="./profile_pic.jpeg" alt="profile pic" />
     </div>
   )

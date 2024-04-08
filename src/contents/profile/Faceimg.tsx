8a88d8df0e74d5cae61c24ed9ac8ed3365ac506a
byungjoo0806 <andybyungjoopark@gmail.com>
2024-04-08 18:11:26 +0900

    profile image size adjusting

--- src/contents/profile/Faceimg.tsx
+++ src/contents/profile/Faceimg.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Faceimg = (props: Props) => {
   return (
-    <div className='w-[40%] md:w-[15%] h-[60%] md:h-[70%] rounded-full overflow-hidden'>
+    <div className='w-32 h-32 rounded-full overflow-hidden'>
         <img className='w-full h-full' src="./profile_pic.png" alt="profile pic" />
     </div>
   )
